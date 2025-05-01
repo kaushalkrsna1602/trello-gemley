f7841a4370d15824151feae9a75a092f49ec6b19
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-05-01 15:26:22 +0530

    backend deployed link added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api' || "",
+  baseURL: 'http://localhost:5000/api' || "https://trello-gemley-backend.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
